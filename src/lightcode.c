06400f89f2e8ee46909b0e06fc5879588ffeba15
unknown <tudoracherobertcosmin@gmail.com>
2022-11-08 13:20:55 +0200

    lightsensor code update

--- src/lightcode.c
+++ src/lightcode.c
@@ -1,27 +1,30 @@
 void setup() {
-  // initialize serial communication at 9600 bits per second:
-  Serial.begin(9600);
+    Serial.begin(9600);
 }
 
 void loop() {
-  // reads the input on analog pin A0 (value between 0 and 1023)
-  int analogValue = analogRead(A0);
+    // reads the input on analog pin A0 (value between 0 and 1023)
+    int analogValue = analogRead(A0);
 
-  Serial.print("Analog reading: ");
-  Serial.print(analogValue);   // the raw analog reading
+    Serial.print("Analog reading = ");
+    Serial.print(analogValue);   // the raw analog reading
 
-  // We'll have a few threshholds, qualitatively determined
-  if (analogValue < 200) {
-    Serial.println(" - Darkest");
-  } else if (analogValue < 400) {
-    Serial.println(" - Dark");
-  } else if (analogValue < 500) {
-    Serial.println(" - Light");
-  } else if (analogValue < 980) {
-    Serial.println(" - Bright");
-  } else {
-    Serial.println(" - Very bright");
-  }
+    // We'll have a few threshholds, qualitatively determined
+    if (analogValue < 10) {
+        Serial.println(" - Dark");
+    }
+    else if (analogValue < 40) {
+        Serial.println(" - Dim");
+    }
+    else if (analogValue < 150) {
+        Serial.println(" - Light");
+    }
+    else if (analogValue < 450) {
+        Serial.println(" - Bright");
+    }
+    else {
+        Serial.println(" - Very Bright");
+    }
 
-  delay(500);
+    delay(500);
 }
